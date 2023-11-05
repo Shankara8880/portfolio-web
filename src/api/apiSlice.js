76c375da6e72f88c723fce225ee74e7d4ee3d6cd
shankar <shankarbevale@sdlccorp.com>
2023-11-05 17:07:16 +0530

    Updated qr-code project link

--- src/api/apiSlice.js
+++ src/api/apiSlice.js
@@ -1,15 +1,15 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const apiSlice = createApi({
     reducerPath: "api",
-    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/api" }),
+    // baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000" }),
     // baseQuery: fetchBaseQuery({ baseUrl: "https://portfolio-production-7da7.up.railway.app/api" }), /api/contact
-    // baseQuery: fetchBaseQuery({ baseUrl: process.env.API_LINK }),
+    baseQuery: fetchBaseQuery({ baseUrl: "https://sbportfolio-backend.netlify.app" }),
     endpoints: builder => {
         return {
             addContact: builder.mutation({
                 query: (contactData) => {
                     return {
-                        url: '/contact/add-contact',
+                        url: '/api/contact/add-contact',
                         method: "POST",
                         body: contactData
                     }
