643ab50bdb22acf26d671c4ca83f716f2fc72650
shankar <shankarbevale@sdlccorp.com>
2023-11-11 13:34:57 +0530

    api Changed

--- src/api/apiSlice.js
+++ src/api/apiSlice.js
@@ -2,8 +2,8 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const apiSlice = createApi({
     reducerPath: "api",
     // baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000" }),
-    // baseQuery: fetchBaseQuery({ baseUrl: "https://portfolio-production-7da7.up.railway.app/api" }), /api/contact
-    baseQuery: fetchBaseQuery({ baseUrl: "https://sbportfolio-backend.netlify.app" }),
+    // baseQuery: fetchBaseQuery({ baseUrl: "https://sbportfolio-backend.netlify.app" }),
+    baseQuery: fetchBaseQuery({ baseUrl: "https://portfolio-801y13tte-shankara8880.vercel.app" }),
     endpoints: builder => {
         return {
             addContact: builder.mutation({
